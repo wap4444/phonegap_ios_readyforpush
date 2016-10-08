ab9439d36f355cfbc8679118eeea2463da1d38c0
wap4444 <wap4444@mail.ru>
2016-10-08 17:38:36 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -64,7 +64,7 @@ var ref = cordova.InAppBrowser.open('http://topstar.vezuedu.kz/fr7/index.php?ipu
 
  function didReceiveRemoteNotificationCallBack(jsonData) {
 
-         alert('RECEIVE: ' + jsonData.payload.body);
+       console.log('RECEIVE: ' + JSON.stringify(jsonData)); 
     }
 function didOpenRemoteNotificationCallBack (jsonData) {
     
